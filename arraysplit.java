295d39e55cc65b3e63eab248651afa4417f1fe95
sv742744 <shashi09op@gmail.com>
2024-12-10 16:12:54 +0530

    Add files via upload

---
+++ arraysplit.java
@@ -0,0 +1,13 @@
+/*
+sha shfc gfh
+[sha, shfc, gfh]
+*/
+import java.util.*;
+class arraysplit{
+    public static void main (String args[]){
+        Scanner s=new Scanner(System.in);
+        String a=s.nextLine();
+        String b[]=a.split(" ");
+        System.out.println(Arrays.toString(b));
+    }
+}
